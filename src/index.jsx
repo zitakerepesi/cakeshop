68e320bcc0779fbca64496ff0d5b86247871bc2e
zita kerepesi <kerepesi.zita@me.com>
2020-10-20 22:20:08 +0100

    basics

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
-}
+// const root = document.getElementById('root');
+// if (root) {
+//   ReactDOM.render(<Hello name="World" />, root);
+// }
